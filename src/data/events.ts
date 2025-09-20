781ba053d8429d25e818b496ed223cd38403f626
snl95 <snl95@txstate.edu>
2025-09-19 19:46:25 -0700

    events update

--- src/data/events.ts
+++ src/data/events.ts
@@ -3,6 +3,24 @@ import type { EventItem } from '@/types/event';
 export const events: EventItem[] = [
 	{
 		id: 1,
+		title: 'GSWS: Player Check-In',
+		info: ['Check In'],
+		location: 'Royal Sonesta (Legends I-III)',
+		googleMapsLink: 'https://maps.app.goo.gl/XQbxxdX6CZ7VKYCEA',
+		date: '9/21/2025',
+		time: '10:00 AM - 4:00 PM',
+	},
+	{
+		id: 2,
+		title: 'GSWS: Player Check-In',
+		info: ['Check In'],
+		location: 'Royal Sonesta (Legends I-III)',
+		googleMapsLink: 'https://maps.app.goo.gl/XQbxxdX6CZ7VKYCEA',
+		date: '9/22/2025',
+		time: '9:00 AM - 3:00 PM',
+	},
+	{
+		id: 3,
 		title: 'GSWS: Opening Stampede',
 		info: ['Wear your Dallas Jersey', 'Trade Jerseys w/ Friends'],
 		location: '713 Music Hall',
@@ -11,7 +29,7 @@ export const events: EventItem[] = [
 		time: '6:00 - 10:00 PM',
 	},
 	{
-		id: 2,
+		id: 4,
 		title: 'GSWS: Friday Night Lights',
 		info: ['TBD'],
 		location: 'Cullen',
@@ -20,12 +38,12 @@ export const events: EventItem[] = [
 		time: '6:00 - 9:00 PM',
 	},
 	{
-		id: 3,
+		id: 5,
 		title: 'GSWS: Closing Ceremonies',
-		info: ['TBD'],
-		location: 'TBD',
-		googleMapsLink: '#',
+		info: ['Awards Presentation', 'Closing Party', 'Celebrate!'],
+		location: 'Bayou Music Center',
+		googleMapsLink: 'https://maps.app.goo.gl/ETESLNCHfzrpLg219',
 		date: '9/27/2025',
-		time: 'TBD',
+		time: '6:00 - 10:00 PM',
 	},
 ];
