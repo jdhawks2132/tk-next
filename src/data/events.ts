3f9e531001b5efa8c671acb8d5ed7d816125d121
snl95 <snl95@txstate.edu>
2025-09-23 18:30:14 -0500

    schedule updates

--- src/data/events.ts
+++ src/data/events.ts
@@ -2,33 +2,6 @@ import type { EventItem } from '@/types/event';
 
 export const events: EventItem[] = [
 	{
-		id: 1,
-		title: 'GSWS: Player Check-In',
-		info: ['Check In'],
-		location: 'Royal Sonesta (Legends I-III)',
-		googleMapsLink: 'https://maps.app.goo.gl/XQbxxdX6CZ7VKYCEA',
-		date: '9/21/2025',
-		time: '10:00 AM - 4:00 PM',
-	},
-	{
-		id: 2,
-		title: 'GSWS: Player Check-In',
-		info: ['Check In'],
-		location: 'Royal Sonesta (Legends I-III)',
-		googleMapsLink: 'https://maps.app.goo.gl/XQbxxdX6CZ7VKYCEA',
-		date: '9/22/2025',
-		time: '9:00 AM - 3:00 PM',
-	},
-	{
-		id: 3,
-		title: 'GSWS: Opening Stampede',
-		info: ['Wear your Dallas Jersey', 'Trade Jerseys w/ Friends'],
-		location: '713 Music Hall',
-		googleMapsLink: 'https://maps.app.goo.gl/1WNZFKJrS8aCzUAt8',
-		date: '9/22/2025',
-		time: '6:00 - 10:00 PM',
-	},
-	{
 		id: 4,
 		title: 'GSWS: Friday Night Lights',
 		info: ['TBD'],
