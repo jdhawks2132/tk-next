9ea5c46ce9e35fd50a522ea80b09d7fc879bb8d1
snl95 <snl95@txstate.edu>
2025-09-25 21:50:34 -0500

    schedule 9.25

--- src/data/events.ts
+++ src/data/events.ts
@@ -2,15 +2,6 @@ import type { EventItem } from '@/types/event';
 
 export const events: EventItem[] = [
 	{
-		id: 4,
-		title: 'GSWS: Friday Night Lights',
-		info: ['TBD'],
-		location: 'Cullen',
-		googleMapsLink: 'https://maps.app.goo.gl/7CXFqQxjyVotGida8',
-		date: '9/26/2025',
-		time: '6:00 - 9:00 PM',
-	},
-	{
 		id: 5,
 		title: 'GSWS: Closing Ceremonies',
 		info: ['Awards Presentation', 'Closing Party', 'Celebrate!'],
