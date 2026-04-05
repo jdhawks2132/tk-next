e4bd1f4b94afe4c8812c7d69df524ec19c59874b
Joshua Hawks <jdhawks@gmail.com>
2026-04-05 08:45:24 -0500

    spring season calendar added

--- src/data/events.ts
+++ src/data/events.ts
@@ -2,17 +2,7 @@ import type { EventItem } from '@/types/event';
 
 export const events: EventItem[] = [
 	{
-		id: 2,
-		title: 'Pegasus Games',
-		info: ['Wear your Jersey', 'Sign In'],
-		location: 'S4',
-		googleMapsLink: 'https://maps.app.goo.gl/cDPPcJjCnP895avG6',
-		infoLink: 'https://www.dallaspssa.org/datesofplay2026',
-		date: '3/22/2026',
-		time: '5:00 PM - 9:00 PM',
-	},
-	{
-		id: 3,
+		id: 1,
 		title: 'Drag & Variety Show',
 		info: ['Wear your Jersey', 'Sign In', 'Be in the Show or Just Watch'],
 		location: 'Round Up Saloon',
