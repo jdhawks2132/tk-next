6a9997ce97dfa6aa06cab4edc0fcd4f4d330ac4b
snl95 <snl95@txstate.edu>
2026-04-27 14:53:00 -0500

    schedule and event updates

--- src/data/events.ts
+++ src/data/events.ts
@@ -3,12 +3,12 @@ import type { EventItem } from '@/types/event';
 export const events: EventItem[] = [
 	{
 		id: 1,
-		title: 'Drag & Variety Show',
-		info: ['Wear your Jersey', 'Sign In', 'Be in the Show or Just Watch'],
-		location: 'Round Up Saloon',
-		googleMapsLink: 'https://maps.app.goo.gl/uiUZV7JxVUY8ZnEd7',
-		infoLink: 'https://www.dallaspssa.org/datesofplay2026',
-		date: '4/19/2026',
-		time: '5:00 PM - 9:00 PM',
+		title: 'Spring Season Awards',
+		info: ['Wear your Jersey', 'Individual Awards', 'Team Awards'],
+		location: 'TBD',
+		googleMapsLink: '#',
+		infoLink: '#',
+		date: '7/19/2026',
+		time: 'TBD',
 	},
 ];
